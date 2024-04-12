c244f023654e0e84611d167b129dbf4d15ada4ba
Mathieu Heurtevin <mathieu.heurtevinruiz@protonmail.com>
2024-04-13 01:33:16 +0200

    WIP: Modification comportement caméra

--- player/player.gd
+++ player/player.gd
@@ -12,7 +12,9 @@ extends CharacterBody2D
 func _physics_process(delta):
 	#Gravity
 	if not is_on_floor():
-		velocity.y += GRAVITY 
+		velocity.y += GRAVITY
+		#$Camera2D.set_position_smoothing_speed(5)
+		#$Camera2D.set_position_smoothing_speed(2)
 	if velocity.y>1000:
 		velocity.y=1000
 
@@ -20,10 +22,12 @@ func _physics_process(delta):
 	# JUMP
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var horizontal_direction=Input.get_axis("move_left","move_right")
+	$Camera2D.set_drag_horizontal_offset(horizontal_direction * 2)
 	if horizontal_direction:
 		velocity.x = horizontal_direction * SPEED
 	else:
