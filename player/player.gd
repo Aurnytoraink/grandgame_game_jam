dc7ddfcdd7f7ab7083e80fd1449d9d5af586b30e
Lwoui <louis.charrais@etu.univ-tours.Fr>
2024-04-13 13:00:59 +0200

    Boutons changement de scène

--- player/player.gd
+++ player/player.gd
@@ -15,12 +15,25 @@ func _physics_process(delta):
 		velocity.y += GRAVITY 
 	if velocity.y>1000:
 		velocity.y=1000
+	
+	
 
+			
 
 	# JUMP
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
 
+
+
+	if Input.is_action_just_pressed("action_nature"):
+		get_tree().change_scene_to_file("res://scenes/worldN.tscn")
+	if Input.is_action_just_pressed("action_urbain"):
+		get_tree().change_scene_to_file("res://scenes/worldU.tscn")
+	if Input.is_action_just_pressed("action_apocalyptic"):
+		get_tree().change_scene_to_file("res://scenes/worldA.tscn")
+			
+			
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var horizontal_direction=Input.get_axis("move_left","move_right")
