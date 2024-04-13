f98055075d2e50eef60d09f9cb592df7cea6448d
YacineHB <95744524+YacineHB@users.noreply.github.com>
2024-04-13 15:23:50 +0200

    fix

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var GRAVITY = 30
 @export var JUMP_VELOCITY = 400.0
 @export var LOWEST_POINT = 6000.0 #Fall -> die
+@export var Player_PosX = position.x
+@export var Player_PosY = position.y
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 
@@ -18,7 +20,8 @@ func _physics_process(delta):
 			get_tree().reload_current_scene()#point de départ
 	if velocity.y>1000:
 		velocity.y=1000
-	
+	Player_PosX = position.x
+	Player_PosY = position.y
 	
 
 			
@@ -27,15 +30,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
 
-
-
-	if Input.is_action_just_pressed("action_nature"):
-		get_tree().change_scene_to_file("res://scenes/worldN.tscn")
-	if Input.is_action_just_pressed("action_urbain"):
-		get_tree().change_scene_to_file("res://scenes/worldU.tscn")
-	if Input.is_action_just_pressed("action_apocalyptic"):
-		get_tree().change_scene_to_file("res://scenes/worldA.tscn")
-			
 			
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
